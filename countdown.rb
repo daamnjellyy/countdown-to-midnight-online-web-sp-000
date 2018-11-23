dee2310b5664c490aaad0f43b134340d0551e027
DANIELLE PARK <dpark1008@gmail.com>
2018-11-23 03:17:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
-def countdown
-end
-
-number = 10
-while countdown
-  puts "#{number} SECOND(S)!"
-  number -= 10
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
