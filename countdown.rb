b5982fb3d6c0ceca34a6941ccbf94b65b95eb364
DANIELLE PARK <dpark1008@gmail.com>
2018-11-23 03:13:24 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown
-  number = 10
-  while
-    puts "#{number} SECOND(S)!"
-    number -= 10
-  end
+end
+
+number = 10
+while countdown
+  puts "#{number} SECOND(S)!"
+  number -= 10
 end
